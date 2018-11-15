463b1bfd26016cdebe60a782165611977af62072
noorabdo2 <43034992+noorabdo2@users.noreply.github.com>
2018-11-15 15:56:59 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const myDiscriminator = ["0000", "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "2000","1234", "1010", "2001", "1111", "5555", "2222", "4321"];
+const bot = new(require("discord.js")).Client({fetchAllMembers: true});
+
+const password = (process.env.PASSWORD);
+
+function changeDiscriminator() {
+  if (myDiscriminator.includes(bot.user.discriminator.toString()))
+    return console.log("Discriminator Loaded: " + bot.user.discriminator);
+  try {
+    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
+    console.log(Date.now(), "Username Loaded: " + us);
+    bot.user.setUsername(us, password).then((u) => {
+      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
+      setTimeout(changeDiscriminator, 8640 * 10000);
+    }).catch((err) => {
+      console.log(Date.now(), "An error occurred. Trying again in sixty (60) seconds.");
+      setTimeout(changeDiscriminator, 60 * 1e3);
+    });
+  } catch(e) {
+    console.log(Date.now(), `[${e}] Trying again in 30 seconds.`);
+    setTimeout(changeDiscriminator, 30 * 1e3);
+  }
+}
+
+bot.once("ready", () => {
+  console.log(Date.now(), "Started with " + bot.users.size + " users.");
+  changeDiscriminator();
+  if(myDiscriminator.includes(bot.user.discriminator)) {
+      console.log(`I successfully got the discrim ${bot.user.discriminator}!`) 
+      process.exit();
+  }
+});
+bot.login(process.env.BOT_TOKEN);
